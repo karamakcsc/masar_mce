e2c1781a7b8bd2962cd7d1462e707be8debdaad0
karamakcsc <karamakcsc@hotmail.com>
2025-09-29 16:50:38 +0300

    update

--- masar_mce/custom/blanket_order/blanket_order.py
+++ masar_mce/custom/blanket_order/blanket_order.py
@@ -3,6 +3,8 @@ from frappe.utils import flt
 
 def validate(self , method):
     calculate_amounts_and_total(self)
+    if self.is_new():
+        get_default_penalty(self)
 
 @frappe.whitelist()
 def get_items_by_supplier(doctype, txt, searchfield, start, page_len, filters):
@@ -23,3 +25,23 @@ def calculate_amounts_and_total(self):
         total_qty += i.qty
     self.custom_total_quantity = total_qty
     self.custom_total = total
+    
+def get_default_penalty(self):
+    all_penalty = frappe.db.sql(
+        """
+        SELECT name, penalty_type, account, penalty_amount, penalty_formula
+        FROM `tabPenalty`
+        WHERE `default` = 1
+          AND `disabled` = 0
+        """,
+        as_dict=True,
+    )
+
+    for p in all_penalty:
+        self.append("custom_penalties", {
+            'penalty': p.name,
+            'penalty_type': p.penalty_type,
+            'account': p.account,
+            'penalty_amount': p.penalty_amount,
+            'penalty_formula': p.penalty_formula
+        })
