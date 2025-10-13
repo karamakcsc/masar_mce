32920c179fff6147402a8c42443b06711b445056
karamakcsc <karamakcsc@hotmail.com>
2025-10-13 15:29:45 +0300

    update

--- masar_mce/custom/quality_inspection/quality_inspection.py
+++ masar_mce/custom/quality_inspection/quality_inspection.py
@@ -22,12 +22,12 @@ def update_blanket_order(self , sa_name):
     for i in sa_doc.items:
         if i.item_code == self.item_code:
             if sa_doc.docstatus:
-            db.set_value(i.doctype , i.name , {
-                'custom_quality_inspection' : self.name , 
-                'custom_quality_inspection_status' : self.status , 
-                'custom_quality_inspection_remarks' : self.remarks , 
-                'custom_quality_inspection_quantity' : self.sample_size} 
-            )
+                db.set_value(i.doctype , i.name , {
+                    'custom_quality_inspection' : self.name , 
+                    'custom_quality_inspection_status' : self.status , 
+                    'custom_quality_inspection_remarks' : self.remarks , 
+                    'custom_quality_inspection_quantity' : self.sample_size} 
+                )
             else: 
                 i.custom_quality_inspection = self.name
                 i.custom_quality_inspection_status =  self.status 
