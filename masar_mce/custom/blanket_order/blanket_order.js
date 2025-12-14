8c57c766ac3a8479fa5dfd3b6908f5f3661162ad
karamakcsc <karamakcsc@hotmail.com>
2025-12-14 14:08:35 +0300

    update selling after tax

--- masar_mce/custom/blanket_order/blanket_order.js
+++ masar_mce/custom/blanket_order/blanket_order.js
@@ -249,3 +249,18 @@ function filterBySupplier(frm) {
         };   
     };
 }
+frappe.ui.form.on("Supplier Agreement Other Terms", {
+    tcs_terms(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (!row.tcs_terms) {
+            row.terms = "";
+            frm.refresh_field("custom_other_terms");
+            return;
+        }
+        frappe.db.get_value("Terms and Conditions", row.tcs_terms, "terms")
+            .then(r => {
+                row.terms = r.message?.terms || "";
+                frm.refresh_field("custom_other_terms");
+            });
+    }
+});
