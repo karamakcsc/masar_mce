101d7b5e02098833423baf04b00271fa3bc91d54
karamakcsc <karamakcsc@hotmail.com>
2026-01-12 09:51:01 +0300

    update role

--- masar_mce/custom/blanket_order/blanket_order.py
+++ masar_mce/custom/blanket_order/blanket_order.py
@@ -247,7 +247,7 @@ def create_pricing_sheet(self):
     })
     for row in rows:
         pricing_sheet.append('items', row)
-    pricing_sheet.save()
+    pricing_sheet.save(ignore_permissions=True)
     pricing_sheet.calculate_pricing_after_tax_and_there_totals()
     pricing_sheet.submit()
     frappe.msgprint(f"Pricing Sheet {pricing_sheet.name} created successfully." , alert=1)
